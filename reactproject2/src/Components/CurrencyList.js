97e2dfd17f1461c092b754059170dac23c37192a
M. Brock Podgurski <mbpod10@gmail.com>
2020-06-25 20:36:07 -0400

    rendered symbols and rates

---
+++ reactproject2/src/Components/CurrencyList.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+
+const CurrencyList = () => {
+  const [currency, getCurrency] = useState([]);
+  const [symbol, getSymbol] = useState("USD");
+  const [date, getDate] = useState("");
+  useEffect(() => {
+    const makeApiCall = async () => {
+      const res = await fetch(
+        `https://api.frankfurter.app/latest?from=${symbol}`
+      );
+      const data = await res.json();
+      console.log("data", data);
+      console.log("date", data.date);
+      getDate(data.date);
+      getCurrency(data.rates);
+    };
+    makeApiCall();
+  }, []);
+
+  console.log("currency", currency);
+
+  const currencyKeyArray = Object.keys(currency).map((element, index) => {
+    return (
+      <h4 key={Math.floor(Math.random() * 10000)}>
+        {element} / {symbol}:
+      </h4>
+    );
+  });
+
+  const currencyValuesArray = Object.values(currency).map((element, index) => {
+    return <h4 key={Math.floor(Math.random() * 10000)}>{element}</h4>;
+  });
+
+  console.log(currencyValuesArray, currencyValuesArray);
+
+  return (
+    <>
+      <h1>
+        Rates Against: {symbol} ({date})
+      </h1>
+      <div className="currency-container">
+        <div>{currencyKeyArray} </div>
+        <div>{currencyValuesArray}</div>
+      </div>
+    </>
+  );
+};
+export default CurrencyList;
