908c524446a1009b3d11c8884eff6d983d0897d0
M. Brock Podgurski <mbpod10@gmail.com>
2020-06-26 11:34:54 -0400

    fixed worksheet

--- reactproject2/src/App.js
+++ reactproject2/src/App.js
@@ -1,13 +1,35 @@
 import React from "react";
+import { Route, Link, Switch, Redirect } from "react-router-dom";
 import "./App.css";
 import CurrencyList from "./Components/CurrencyList";
 import CurrencyHome from "./Components/CurrencyHome";
+import About from "./Components/About";
 
 function App() {
   return (
     <div className="App">
-      <CurrencyHome />
-      <CurrencyList />
+      <nav className="nav-box">
+        <Link to="/">
+          <h1>Home</h1>
+        </Link>
+        <Link to="/about">
+          <h1>About</h1>
+        </Link>
+        <Link to="/currencies">
+          <h1>Currencies</h1>
+        </Link>
+      </nav>
+      <main>
+        <Switch>
+          <Route exact path="/" component={CurrencyHome} />
+          <Route exact path="/about" component={About} />
+          <Route
+            exact
+            pather="/currencies"
+            render={(routerProps) => <CurrencyList {...routerProps} />}
+          />
+        </Switch>
+      </main>
     </div>
   );
 }
