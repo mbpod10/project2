eea772ce530e7eb5328ec0c17f02a5ff85d3efe4
M. Brock Podgurski <mbpod10@gmail.com>
2020-06-30 12:43:23 -0400

    Updates

---
+++ precache-manifest.1461bc5689de3000bf7bd714271dcc60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c63f8f628811f5eb9f9ebd7e16c5a13d",
+    "url": "/project2/index.html"
+  },
+  {
+    "revision": "e112790ff5978c1fdc54",
+    "url": "/project2/static/css/main.dbcc091f.chunk.css"
+  },
+  {
+    "revision": "9822733efd2f7706ab45",
+    "url": "/project2/static/js/2.7e5f025b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/project2/static/js/2.7e5f025b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e112790ff5978c1fdc54",
+    "url": "/project2/static/js/main.3b878f68.chunk.js"
+  },
+  {
+    "revision": "e97fcc83ec8a3a645207",
+    "url": "/project2/static/js/runtime-main.b6816f33.js"
+  }
+]);
